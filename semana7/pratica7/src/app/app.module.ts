3e70b9836164b652ddf1f126763fda8941a767dc
Welvis <welvisconta@gmail.com>
2024-01-17 09:29:06 -0300

    ajustes pratica 7

--- semana7/pratica7/src/app/app.module.ts
+++ semana7/pratica7/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CarrinhoComponent } from './carrinho/carrinho.component';
+import { ClassesComponent } from './classes/classes.component';
+import { ObjetosComponent } from './objetos/objetos.component';
+import { PropriedadesComponent } from './propriedades/propriedades.component';
+import { ValorPropriedadeComponent } from './valor-propriedade/valor-propriedade.component';
+import { SearchJsonComponent } from './search-json/search-json.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarrinhoComponent,
+    ClassesComponent,
+    ObjetosComponent,
+    PropriedadesComponent,
+    ValorPropriedadeComponent,
+    SearchJsonComponent
   ],
   imports: [
     BrowserModule
