27e2982cfbbbcd63437c0cc9597f199923e9acdb
macbook <macbook@MacBook-Air-de-macbook.local>
2024-03-05 21:54:56 -0300

    p14

---
+++ pratica14/PetConsult/src/app/Services/auth.service.ts
@@ -0,0 +1,96 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, tap } from 'rxjs';
+import { Usuario } from '../Components/models/usuario.model';
+
+interface AuthResponseData {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  usuario = new BehaviorSubject<Usuario>(new Usuario('', '', '', new Date()));
+
+  constructor(private http: HttpClient) { }
+  signupUser(email: string, password: string) {
+    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAIIWx8fddj0hzxJw8rJXIiOO-bMIlVfTc',
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      }).pipe(
+        tap(resData => {
+          const expiracaoData = new Date(new Date().getTime() + +resData.expiresIn * 1000);
+          const usuario = new Usuario(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            expiracaoData
+          );
+
+          this.usuario.next(usuario);
+          localStorage.setItem('userData', JSON.stringify(usuario));
+        })
+      );
+  }
+
+  loginUser(email: string, password: string) {
+    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAIIWx8fddj0hzxJw8rJXIiOO-bMIlVfTc',
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      }).pipe(
+        tap(resData => {
+          const expiracaoData = new Date(new Date().getTime() + +resData.expiresIn * 1000);
+          const usuario = new Usuario(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            expiracaoData
+          );
+          this.usuario.next(usuario);
+          localStorage.setItem('userData', JSON.stringify(usuario));
+        }),
+      );
+  }
+
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string;
+
+    } = JSON.parse(localStorage.getItem('userData') as string);
+    if (!userData) {
+      return;
+    }
+
+    const loadedUser = new Usuario(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExpirationDate)
+    );
+
+    if (loadedUser.token) {
+      this.usuario.next(loadedUser);
+    }
+
+
+  }
+
+  logout() {
+    this.usuario.next(new Usuario('', '', '', new Date()));
+
+  }
+
+}
